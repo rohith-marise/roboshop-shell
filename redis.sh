0eb9a0311298a5b7e64a6954f8bc12faa3b929ef
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-07-31 01:29:46 +0530

    rollback Updated set-hostname

--- redis.sh
+++ redis.sh
@@ -3,5 +3,6 @@ yum module enable redis:remi-6.2 -y
 yum install redis -y
 # Update listen address from 127.0.0.1 to 0.0.0.0
 # /etc/redis.conf & /etc/redis/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 systemctl enable redis
 systemctl restart redis
