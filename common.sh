105abc41d903da2be313f18d3c51259fb54fa8db
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-03 11:37:51 +0530

    Creating functions for common things in the roboshop application

--- common.sh
+++ common.sh
@@ -1,33 +1,79 @@
+func_apppreq() {
+   echo -e "\e[35m >>>>>> Create ${component} Service <<<<< \e[0m"
+   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+   echo -e "\e[35m >>>>>> Create Application User <<<<< \e[0m"
+   useradd roboshop &>>${log}
+   echo -e "\e[35m >>>>>> Clean up existing App Content <<<<< \e[0m"
+   rm -rf /app &>>${log}
+   echo -e "\e[35m >>>>>> Create App directory <<<<< \e[0m"
+   mkdir /app &>>${log}
+   echo -e "\e[35m >>>>>> Download Application content <<<<< \e[0m"
+   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+   echo -e "\e[35m >>>>>> Extract Application Content <<<<< \e[0m"
+   cd /app &>>${log}
+   unzip /tmp/${component}.zip &>>${log}
+   cd /app &>>${log}
+}
+
+func_systemd() {
+  echo -e "\e[35m >>>>>> Start ${component} Service <<<<< \e[0m"
+    systemctl daemon-reload &>>${log}
+    systemctl enable ${component} &>>${log}
+    systemctl restart ${component} &>>${log}
+}
+
 func_nodejs() {
   log=/tmp/roboshop.log
-  echo -e "\e[35m >>>>>> Create ${component} Service <<<<< \e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
   echo -e "\e[35m >>>>>> Create MongoDB Repo <<<<< \e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
   echo -e "\e[35m >>>>>> Install Nodejs Repo <<<<< \e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
   echo -e "\e[35m >>>>>> Install Nodejs <<<<< \e[0m"
   yum install nodejs -y &>>${log}
-  echo -e "\e[35m >>>>>> Create Application User <<<<< \e[0m"
-  useradd roboshop &>>${log}
-  echo -e "\e[35m >>>>>> >>>>>> Removing Existing APP Content <<<<< \e[0m"
-  rm -rf /app &>>${log}
-  echo -e "\e[35m >>>>>> Create App directory <<<<< \e[0m"
-  mkdir /app &>>${log}
-  echo -e "\e[35m >>>>>> Download Application content <<<<< \e[0m"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  echo -e "\e[35m >>>>>> Extract Application Content <<<<< \e[0m"
-  cd /app &>>${log}
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app &>>${log}
+
+  func_apppreq
+
   echo -e "\e[35m >>>>>> Download Nodejs Dependencies <<<<< \e[0m"
   npm install &>>${log}
   echo -e "\e[35m >>>>>> Install Mongo Client <<<<< \e[0m"
   yum install mongodb-org-shell -y &>>${log}
   echo -e "\e[35m >>>>>> Load ${component} Schema <<<<< \e[0m"
   mongo --host mongodb.devrohiops.online </app/schema/${component}.js &>>${log}
-  echo -e "\e[35m >>>>>> Start ${component} Service <<<<< \e[0m"
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl restart ${component} &>>${log}
+
+  func_systemd
+
+}
+
+func_java() {
+
+ echo -e "\e[35m >>>>>> Install Maven <<<<< \e[0m"
+ yum install maven -y &>>${log}
+
+ func_apppreq
+
+ echo -e "\e[35m >>>>>> build ${component} Service <<<<< \e[0m"
+ mvn clean package &>>${log}
+ mv target/${component}-1.0.jar ${component}.jar &>>${log}
+
+ echo -e "\e[35m >>>>>> Install Mysql Client <<<<< \e[0m"
+ #Mysql comes with centos default for client we can install any version
+ yum install mysql -y &>>${log}
+ echo -e "\e[35m >>>>>> Load ${component} Schema <<<<< \e[0m"
+ mysql -h mysql.devrohiops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+
+ func_systemd
+
+}
+
+func_python() {
+
+  echo -e "\e[35m >>>>>> Install python packages <<<<< \e[0m"
+  yum install python36 gcc python3-devel -y &>>${log}
+
+  func_apppreq
+
+  echo -e "\e[35m >>>>>> Build ${component}  <<<<< \e[0m"
+  pip3.6 install -r requirements.txt &>>${log}
+
+  func_systemd
 }
--- shipping.sh
+++ shipping.sh
@@ -1,18 +1,4 @@
-cp shipping.service /etc/systemd/system/shipping.service
-yum install maven -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-systemctl daemon-reload
+component=shipping
+source common.sh
 
-#Mysql comes with centos defaultly for client we can install any version
-yum install mysql -y
-mysql -h mysql.devrohiops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-systemctl enable shipping
-systemctl restart shipping
+func_java
