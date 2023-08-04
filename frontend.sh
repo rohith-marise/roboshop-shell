9a970b97292af622a9029cee260a2712b7f14662
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-04 10:19:20 +0530

    Exit status and comments

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,27 @@
-yum install nginx -y
+log=/tmp/roboshop.log
+echo -e "\e[35m >>>>>> Installing Nginx Server <<<<< \e[0m"
+yum install nginx -y &>>${log}
 echo $?
 
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[35m >>>>>> Adding Reverse proxy configuration <<<<< \e[0m"
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 echo $?
 
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[35m >>>>>> Removing Existing content <<<<< \e[0m"
+rm -rf /usr/share/nginx/html/* &>>${log}
 echo $?
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo $?
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "\e[35m >>>>>> Downloading the Application content <<<<< \e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 echo $?
 
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[35m >>>>>> Unzipping the content <<<<< \e[0m"
+cd /usr/share/nginx/html &>>${log}
+unzip /tmp/frontend.zip &>>${log}
+echo $?
+
+echo -e "\e[35m >>>>>> Restarting The Services <<<<< \e[0m"
+systemctl enable nginx &>>${log}
+systemctl restart nginx &>>${log}
 echo $?
 
 
