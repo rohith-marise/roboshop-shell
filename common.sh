32c06162877d95e4a5c64ee11dceeec3b44588a9
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-04 10:25:53 +0530

    Exit status

--- common.sh
+++ common.sh
@@ -1,21 +1,36 @@
 log=/tmp/roboshop.log
 
+func_exit_status() {
+  if [ $? -eq 0 ] ; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31 FAILED \e[0m"
+  fi
+  echo $?
+}
 func_apppreq() {
 
    echo -e "\e[35m >>>>>> Create ${component} Service <<<<< \e[0m"
    cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+   func_exit_status
    echo -e "\e[35m >>>>>> Create Application User <<<<< \e[0m"
    useradd roboshop &>>${log}
+   func_exit_status
    echo -e "\e[35m >>>>>> Clean up existing App Content <<<<< \e[0m"
    rm -rf /app &>>${log}
+   func_exit_status
    echo -e "\e[35m >>>>>> Create App directory <<<<< \e[0m"
    mkdir /app &>>${log}
+   func_exit_status
    echo -e "\e[35m >>>>>> Download Application content <<<<< \e[0m"
    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+   func_exit_status
    echo -e "\e[35m >>>>>> Extract Application Content <<<<< \e[0m"
    cd /app &>>${log}
+   func_exit_status
    unzip /tmp/${component}.zip &>>${log}
    cd /app &>>${log}
+   func_exit_status
 }
 
 func_systemd() {
@@ -23,6 +38,7 @@ func_systemd() {
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
+    func_exit_status
 }
 
 func_schema_steup() {
@@ -30,31 +46,39 @@ func_schema_steup() {
   if [ "${schema_type}" == "mongodb" ] ; then
     echo -e "\e[35m >>>>>> Install Mongo Client <<<<< \e[0m"
     yum install mongodb-org-shell -y &>>${log}
+    func_exit_status
     echo -e "\e[35m >>>>>> Load ${component} Schema <<<<< \e[0m"
     mongo --host mongodb.devrohiops.online </app/schema/${component}.js &>>${log}
+    func_exit_status
    fi
 
    if [ "{schema_type}" == "mysql" ] ; then
       echo -e "\e[35m >>>>>> Install Mysql Client <<<<< \e[0m"
       #Mysql comes with centos default for client we can install any version
       yum install mysql -y &>>${log}
+      func_exit_status
       echo -e "\e[35m >>>>>> Load ${component} Schema <<<<< \e[0m"
       mysql -h mysql.devrohiops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+      func_exit_status
       fi
 }
 
 func_nodejs() {
     echo -e "\e[35m >>>>>> Create MongoDB Repo <<<<< \e[0m"
     cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+    func_exit_status
     echo -e "\e[35m >>>>>> Install Nodejs Repo <<<<< \e[0m"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+    func_exit_status
     echo -e "\e[35m >>>>>> Install Nodejs <<<<< \e[0m"
     yum install nodejs -y &>>${log}
+    func_exit_status
 
     func_apppreq
 
     echo -e "\e[35m >>>>>> Download Nodejs Dependencies <<<<< \e[0m"
     npm install &>>${log}
+    func_exit_status
 
     func_schema_steup
 
@@ -65,13 +89,14 @@ func_java() {
 
  echo -e "\e[35m >>>>>> Install Maven <<<<< \e[0m"
  yum install maven -y &>>${log}
+ func_exit_status
 
  func_apppreq
 
  echo -e "\e[35m >>>>>> build ${component} Service <<<<< \e[0m"
  mvn clean package &>>${log}
  mv target/${component}-1.0.jar ${component}.jar &>>${log}
-
+ func_exit_status
  func_schema_steup
 
  func_systemd
@@ -82,11 +107,13 @@ func_python() {
 
   echo -e "\e[35m >>>>>> Install python packages <<<<< \e[0m"
   yum install python36 gcc python3-devel -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
   echo -e "\e[35m >>>>>> Build ${component}  <<<<< \e[0m"
   pip3.6 install -r requirements.txt &>>${log}
+  func_exit_status
 
   func_systemd
 }
@@ -94,6 +121,7 @@ func_python() {
 func_golang() {
   echo -e "\e[35m >>>>>> Install Golang packages <<<<< \e[0m"
   yum install golang -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
@@ -101,6 +129,7 @@ func_golang() {
   go mod init ${component} &>>${log}
   go get &>>${log}
   go build &>>${log}
+  func_exit_status
   
   func_systemd
 
--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,29 @@
 log=/tmp/roboshop.log
+source common.sh
+
 echo -e "\e[35m >>>>>> Installing Nginx Server <<<<< \e[0m"
 yum install nginx -y &>>${log}
-echo $?
+
 
 echo -e "\e[35m >>>>>> Adding Reverse proxy configuration <<<<< \e[0m"
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
-echo $?
+func_exit_status
 
 echo -e "\e[35m >>>>>> Removing Existing content <<<<< \e[0m"
 rm -rf /usr/share/nginx/html/* &>>${log}
-echo $?
+func_exit_status
 echo -e "\e[35m >>>>>> Downloading the Application content <<<<< \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
-echo $?
+func_exit_status
 
 echo -e "\e[35m >>>>>> Unzipping the content <<<<< \e[0m"
 cd /usr/share/nginx/html &>>${log}
 unzip /tmp/frontend.zip &>>${log}
-echo $?
+func_exit_status
 
 echo -e "\e[35m >>>>>> Restarting The Services <<<<< \e[0m"
 systemctl enable nginx &>>${log}
 systemctl restart nginx &>>${log}
-echo $?
+func_exit_status
 
 
