cb8f8b276d496e7f535aad4e43a8be76af6601fa
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-04 11:12:42 +0530

    Password NFR

--- payment.sh
+++ payment.sh
@@ -1,7 +1,7 @@
 component=payment
 source common.sh
 rabbitmq_app_password=$1
-if [ -z ${ rabbitmq_app_password} ] ; then
+if [ -z ${rabbitmq_app_password} ] ; then
   echo -e "\e[31m RabbitMQ AppUser Password Missing \e[0m"
   exit 1
 fi
