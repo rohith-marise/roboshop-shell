98742b71b5812fcdcb46be3db34bcd498aa00fae
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-04 10:14:20 +0530

    Exit status

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,19 @@
 yum install nginx -y
+echo $?
 
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo $?
 
 rm -rf /usr/share/nginx/html/*
+echo $?
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo $?
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo $?
 
 systemctl enable nginx
 systemctl restart nginx
+echo $?
 
 
